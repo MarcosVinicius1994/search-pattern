fcbfcbd5f4dbf81e1df46e0cf317f413aeefd4c9
marcos.nunes <marcosvtn@yahoo.com.br>
2022-06-04 16:18:48 -0300

    Adicionando tratamento de erro para a leitura dos arquivos

--- src/app/utils/search-pattern-util/read-folder-util.js
+++ src/app/utils/search-pattern-util/read-folder-util.js
@@ -1,19 +1,20 @@
-// const { printFile } = require('./print-info-util')
-// const { parseLogs } = require('./parse-lines-util')
-
 module.exports = {
     async redFilesLine(fs, path, word) {
-        let lines = fs.readFileSync(path).toString().split("\n")
-        let searchLine = ''
-        let statusSearchLine = false
-        for (let i = 0; i < lines.length; i++) {
-            searchLine = lines[i].match(word)
-            if (!!searchLine) {
-                if (searchLine[0] === word) {
-                    statusSearchLine = true
+        try {
+            let lines = fs.readFileSync(path).toString().split("\n")
+            let searchLine = ''
+            let statusSearchLine = false
+            for (let i = 0; i < lines.length; i++) {
+                searchLine = lines[i].match(word)
+                if (!!searchLine) {
+                    if (searchLine[0] === word) {
+                        statusSearchLine = true
+                    }
                 }
             }
+            return statusSearchLine
+        } catch (error) {
+            throw new Error(`Error ao realizar a leitura do arquivo: + ${lines} +  para o termo: + ${word} `)
         }
-        return statusSearchLine
     }
 }
