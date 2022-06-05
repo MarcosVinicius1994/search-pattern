a1cdb3d49843e4cab70869df3bbef071cc872210
marcos.nunes <marcosvtn@yahoo.com.br>
2022-06-05 16:25:53 -0300

    Alteração da service para receber o diretorio de arquivos

--- src/app/services/search-pattern-batch-service.js
+++ src/app/services/search-pattern-batch-service.js
@@ -1,9 +1,10 @@
 const { readFile } = require('../utils/search-pattern-util/read-files-util')
+const data = 'src/data/*.txt'
 
 module.exports = {
     async loadSearchPatternService(word) {
         try {
-            await readFile(word)
+            await readFile(word, data)
         } catch (error) {
             return {
                 statusCode: 500,
